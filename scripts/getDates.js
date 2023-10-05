55cb4e6efce3dc3f91787a6b543ec8ffe1596708
assan <chembeassan@yahoo.com>
2023-10-05 22:52:32 +0200

    changes and new discover page added

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,7 +1,19 @@
-// Get the current year and populate it in the first paragraph of the footer
-const currentYear = new Date().getFullYear();
-document.querySelector('footer p:first-child span').textContent = currentYear;
+// Store the current date in localStorage
+localStorage.setItem("lastVisit", Date.now().toString());
+// Function to get the current date in the format "DD Month YYYY"
+function getCurrentDate() {
+  var options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date().toLocaleDateString(undefined, options);
+}
 
-// Get the last modified date and populate it in the second paragraph of the footer
-const lastModified = document.lastModified;
-document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+// Find the span element with the id "lastModified" within the footer
+var lastModifiedSpan = document.getElementById("lastModified");
+
+// Update the content of the span element with the current date
+lastModifiedSpan.textContent = getCurrentDate();
+
+// Visit counter using localStorage
+let visitCounter = parseInt(localStorage.getItem('visitCounter')) || 0;
+visitCounter++;
+localStorage.setItem('visitCounter', visitCounter);
+document.getElementById('visitCounter').textContent = visitCounter;
