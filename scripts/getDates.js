c2349ebfa6167c79545c009e9cecc50d13ba57e5
Chembeassan <chembeassan@yahoo.com>
2024-11-12 10:25:19 +0200

    Second commit: Created  CSS, images, and JS files plus added some contents it.

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+// Get the current year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Get the last modified date
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
