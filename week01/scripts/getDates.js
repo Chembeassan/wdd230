dc298e2a1c2d877084d294ca7db0716c362fab7a
Chembeassan <chembeassan@yahoo.com>
2024-11-24 07:48:09 +0200

    week01 wireframe, chamber home page added

--- week01/scripts/getDates.js
+++ week01/scripts/getDates.js
@@ -11,3 +11,12 @@ if (lastModified) {
     const modifiedDate = document.lastModified;
     lastModified.textContent = `Last updated: ${modifiedDate}`;
 }
+
+function toggleMenu() {
+    const navList = document.querySelector('nav ul');
+    const hamburger = document.querySelector('.hamburger');
+
+    // Toggle the 'active' class on both elements
+    navList.classList.toggle('active');
+    hamburger.classList.toggle('active');
+}
